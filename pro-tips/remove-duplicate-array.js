6c573134bdc52a33d6c3d1a06c01b99cd6b19a33
jaeseugi <jaeseugi.1927@gmail.com>
2021-12-18 19:12:06 +0900

    Set을 이용한 중복제거 후 복사

---
+++ pro-tips/remove-duplicate-array.js
@@ -0,0 +1,8 @@
+'use strict'
+// Remove Duplicates!
+// Array 자료구조는 중복을 허용
+const array = ['🐶', '🐱', '🐈', '🐶', '🦮', '🐱'];
+console.log(array);
+
+// Set 자료구조는 중복을 허용
+console.log([...new Set(array)]);
